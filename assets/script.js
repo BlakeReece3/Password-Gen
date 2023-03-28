de73bc0f46c87cbe3e1096ae3a911ed873b540e7
Blake Reece <frankiereece3@gmail.com>
2023-03-28 13:17:11 -0700

    finalized script and debugged

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,62 @@
-// Creates funtion for password generating
-function generatePassword() {
 
-// Creating variables for password
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "`", "+","=", "?", ";"];
-var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
-// prompts to confirm input
- var lowercaseCheck =confirm('Click OK to include lowercase characters.');
- var uppercaseCheck = confirm('Click OK to include uppercase characters.');
- var symbolCheck = confirm('Click OK to include symbols.');
- var numberCheck = confirm('Click OK to include numbers.');
-
-// Creates prompt to enter value 
-var passwordValue = window.prompt("Please select length between 8 and 128 characters.");
-// Creates if fuction
-
-if (passwordValue >=8 && passwordValue <=128) {
-  console.log(passwordValue)
-}
-}
-// Writes password
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
 // Creates variable for generate id
 var generateBtn = document.querySelector("#generate");
 
-const finalChoices = [];
 
-if (lowercaseCheck === true) {
-  finalChoices.push.apply(finalChoices, lowercase);
+// Writes password
+function writePassword() {
+  var password = generatePassword();
+  var passwordPrint = document.querySelector("#password");
+  passwordPrint.value = password;
 }
-if (uppercaseCheck === true) {
-  finalChoices.push.apply(finalChoices, uppercase);
-}
-if (symbolCheck === true) {
-  finalChoices.push.apply(finalChoices, symbol);
-}
-if (numberCheck === true) {
-  finalChoices.push.apply(finalChoices, number);
-}
-
-// Adds event listener to generate button
+// Adds event listener to button
 generateBtn.addEventListener("click", writePassword);
+
+// Creates password generating function
+function generatePassword() {
+
+  // Creating variables for password
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "`", "+","=", "?", ";"];
+  var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+// Creates prompt to enter value 
+  var passwordValue = window.prompt("Please select between 8 and 128 characters.");
+
+  // Prompts to confirm input
+  var lowercaseCheck = confirm('Click OK to include lowercase characters.');
+  var uppercaseCheck = confirm('Click OK to include uppercase characters.');
+  var symbolCheck = confirm('Click OK to include symbols.');
+  var numberCheck = confirm('Click OK to include numbers.');
+
+  
+  // Creates if fuction
+
+  if (passwordValue >=8 && passwordValue <=128) {
+  console.log(passwordValue)
+
+  const finalChoices = [];
+
+  if (lowercaseCheck === true) {
+    finalChoices.push.apply(finalChoices, lowercase);
+  }
+  if (uppercaseCheck === true) {
+    finalChoices.push.apply(finalChoices, uppercase);
+  }
+  if (symbolCheck === true) {
+    finalChoices.push.apply(finalChoices, symbol);
+  }
+  if (numberCheck === true) {
+    finalChoices.push.apply(finalChoices, number);
+  }
+
+  var userPassword = ""
+
+  for (i = 0; i < passwordValue; i++) {
+    userPassword += finalChoices[Math.floor(Math.random() * finalChoices.length)]
+  };
+
+  return userPassword;
+  }
+};
+
