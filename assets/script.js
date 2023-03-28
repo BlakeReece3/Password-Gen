bd8a97316613f6a49b0414c6e0d3044be160ae44
Blake Reece <frankiereece3@gmail.com>
2023-03-28 15:18:13 -0700

    Changed if, simplified vars

--- assets/script.js
+++ assets/script.js
@@ -2,24 +2,15 @@
 // Creates variable for generate id
 var createBtn = document.querySelector("#generate");
 
-
-// Writes password
-function writePassword() {
-  var password = generatePassword();
-  var passwordPrint = document.querySelector("#password");
-  passwordPrint.value = password;
-}
-// Adds event listener to button
-createBtn.addEventListener("click", writePassword);
-
 // Creates password generating function
 function generatePassword() {
 
   // Creating variables for password
-  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "`", "+","=", "?", ";"];
-  var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+  var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var symbol = '!@#$%^&*~`+=?;';
+  var number = '1234567890';
+  
 // Creates prompt to enter value 
   var passwordValue = window.prompt("Please select between 8 and 128 characters.");
 
@@ -37,17 +28,17 @@ function generatePassword() {
 
   const finalChoices = [];
 
-  if (lowercaseCheck === true) {
-    finalChoices.push.apply(finalChoices, lowercase);
+  if (lowercaseCheck) {
+    finalChoices.push(...lowercase);
   }
-  if (uppercaseCheck === true) {
-    finalChoices.push.apply(finalChoices, uppercase);
+  if (uppercaseCheck) {
+    finalChoices.push(...uppercase);
   }
-  if (symbolCheck === true) {
-    finalChoices.push.apply(finalChoices, symbol);
+  if (symbolCheck) {
+    finalChoices.push(...symbol);
   }
-  if (numberCheck === true) {
-    finalChoices.push.apply(finalChoices, number);
+  if (numberCheck) {
+    finalChoices.push(...number);
   }
 
   var userPassword = ""
@@ -59,4 +50,11 @@ function generatePassword() {
   return userPassword;
   }
 };
-
+// Adds event listener to button
+createBtn.addEventListener("click", writePassword);
+// Writes password
+function writePassword() {
+  var password = generatePassword();
+  var passwordPrint = document.querySelector("#password");
+  passwordPrint.value = password;
+}
